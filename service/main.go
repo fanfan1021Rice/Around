7fd158c1c354939bb8c1f4355f46a3cbbccba547
Yanlin <fanylcc@gmail.com>
2018-05-19 17:39:20 -0500

    update

--- service/main.go
+++ service/main.go
@@ -15,6 +15,7 @@ import (
 	"github.com/auth0/go-jwt-middleware"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
+	"cloud.google.com/go/bigtable"
 	//"golang.org/x/oauth2/jwt"
 )
 
@@ -91,10 +92,10 @@ const (
 	TYPE = "post"
 	DISTANCE = "200km"
 	// Needs to update
-	//PROJECT_ID = "around-xxx"
-	//BT_INSTANCE = "around-post"
+	PROJECT_ID = "around-203914"
+	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.232.157.194:9200"
+	ES_URL = "http://35.226.210.148:9200"
 	BUCKET_NAME = "post-images-203914"
 )
 
@@ -202,6 +203,30 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	defer file.Close()
 
 	ctx := context.Background()
+	// you must update project name here
+	// Save to BigTable
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+
+	// save data to a row(mutation)
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 
 	// replace it with your real bucket name.
 	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
@@ -216,10 +241,8 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 
 	// Save to ES.
 	saveToES(p, id)
-
-	// Save to BigTable.
-	//saveToBigTable(p, id)
 }
+
 // Save a post to ElasticSearch
 func saveToES(p *Post, id string) {
 	// Create a client
